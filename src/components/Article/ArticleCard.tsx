dc6206809e8b78c9636ff240eb284386bc709e4b
Michal Kowalewski <michalkowalewski88@gmail.com>
2023-04-11 13:07:01 +0200

    ad styles and restructure single article card

--- src/components/Article/ArticleCard.tsx
+++ src/components/Article/ArticleCard.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import './ArticleCard.scss'
-import { Article } from "../../types/types";
-
+import {Article} from "../../types/types";
 
 
 interface Props {
     article: Article;
 }
 
-const ArticleCard: React.FC<Props> = ({ article }) => {
+const ArticleCard: React.FC<Props> = ({article}) => {
     return (
-        <div className={"article"} key={article.title}>
+        <div className="article" key={article.title}>
+            <a className="article__link" target="_blank" href={article.url}>
+            {article.urlToImage ?
+                <div className="article__image-container">
+                    <img className="article__image" src={`${article.urlToImage}`} alt="article image"/>
+                </div>
+            : <div className="article__image-placeholder"/>
+            }
             <p>{article.title}</p>
             {article.description && <p>{article.description}</p>}
-            <a target="_blank" href={article.url}>Link</a>
+            </a>
         </div>
     );
 }
