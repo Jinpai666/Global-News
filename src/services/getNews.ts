3113bc30e2439c0c8ec66d1dbc33bab39414b4ad
Michal Kowalewski <michalkowalewski88@gmail.com>
2023-04-13 16:49:18 +0200

    cors fix

--- src/services/getNews.ts
+++ src/services/getNews.ts
@@ -1,7 +1,11 @@
-import axios from 'axios';
-import { Article } from '../types/types';
+import axios, { AxiosRequestConfig } from 'axios';
+import { Article } from '../types/types'
 
 const apiKey = '05e45c4cb822480891d2b84cef40502f';
+
+interface CustomAxiosRequestConfig extends AxiosRequestConfig {
+    mode?: string;
+}
 
 export interface ApiResponse {
     status: string;
@@ -10,9 +14,19 @@ export interface ApiResponse {
 }
 
 export async function getNews(): Promise<Article[]> {
-    const response = await axios.get<ApiResponse>(
-        `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`,
-        { mode: 'cors' }
-    );
-    return response.data.articles;
+    try {
+        const options: CustomAxiosRequestConfig = {
+            mode: 'cors',
+        };
+
+        const response = await axios.get<ApiResponse>(
+            `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`,
+            options
+        );
+
+        return response.data.articles;
+    } catch (error) {
+        console.log(error);
+        return [];
+    }
 }
