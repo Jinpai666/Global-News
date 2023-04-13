2b2fcd2eb5ca2ad386bbabac044de95a03f9a922
Michal Kowalewski <michalkowalewski88@gmail.com>
2023-04-13 16:48:24 +0200

    cors test

--- src/services/getNews.ts
+++ src/services/getNews.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
-import { Article } from '../types/types'
+import { Article } from '../types/types';
 
 const apiKey = '05e45c4cb822480891d2b84cef40502f';
-
 
 export interface ApiResponse {
     status: string;
@@ -11,15 +10,9 @@ export interface ApiResponse {
 }
 
 export async function getNews(): Promise<Article[]> {
-    try {
-        const response = await axios.get<ApiResponse>(
-
-
-            `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`,
-        );
-        return response.data.articles;
-    } catch (error) {
-        console.log(error);
-        return [];
-    }
+    const response = await axios.get<ApiResponse>(
+        `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`,
+        { mode: 'cors' }
+    );
+    return response.data.articles;
 }
