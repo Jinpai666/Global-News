9fabe4c0110aa2d9d6ba607eecc50cd701625264
Michal Kowalewski <michalkowalewski88@gmail.com>
2023-04-25 13:43:44 +0200

    create clock component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Footer.scss'
+import CurrentTime from "../Clock/Clock";
 
 
 const Footer :React.FC = () => {
     return (
         <footer className="footer">
-            <div>Aktualna godzina: 00:00:00</div>
+            <div><CurrentTime/></div>
             <div> Liczba artykułów</div>
         </footer>
     );
