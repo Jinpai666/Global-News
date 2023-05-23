43c7d3ae889ae67c80319f2bad05b20ec4277666
Michal Kowalewski <michalkowalewski88@gmail.com>
2023-05-23 12:18:25 +0200

    article display change

--- src/components/ArticleListItem/ArticleListItem.tsx
+++ src/components/ArticleListItem/ArticleListItem.tsx
@@ -6,13 +6,25 @@ import {Article} from "../../types/types";
 type Props = {
     article: Article;
 }
-
 const ArticleListItem: React.FC<Props> = ({article}) => {
+
+    const date = article.pubDate ? new Date(article.pubDate) : null;
+    const formattedDate = date?.toLocaleDateString('pl-PL');
+    const formattedTime = date?.toLocaleTimeString('pl-PL');
+
     return (
         <div className="list">
             <h2 className="list__title">{article.title}</h2>
             <p>{article.description}</p>
-            <a href={article.link}>Link</a>
+            <div className="list__bottom">
+                <a href={article.link}>Link</a>
+                <div className="list__bottom-date">
+                    <div>{formattedDate}&nbsp;|&nbsp;</div>
+                    <div>{formattedTime}</div>
+                </div>
+
+
+            </div>
         </div>
     );
 }
--- src/types/types.ts
+++ src/types/types.ts
@@ -6,6 +6,7 @@ export type Article = {
         image_url?: string;
         publishedAt: string;
         content?: string;
+        pubDate?: string;
 
 }
 export type ApiResponse = {
